972c888e65ffe999ac91f27d554252529c967440
shaikjameel3786-stack <shaikjameel3786@gmail.com>
2025-09-25 15:00:48 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,8 @@
+#example for int data types 
+val1 =int(input("Enter val1: "))
+val2=int(input("Enter val2: "))
+print("val1 + val2= ", val1 + val2) # Addition
+print("val1 - -val2 =", val1 - val2) # Substration
+print("val1 * val2 =", val1 * val2 )# Multiplication
+print("val1 / val2 =", val1 / val2)# Division
+
